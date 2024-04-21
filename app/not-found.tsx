eb98d10fbcc17dc621ab5ba573df3594581a0d36
JeremyDallain <jeremy.dallain@gmail.com>
2024-04-21 19:19:24 +0200

    routing

---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NotFoundPage = () => {
+  return (
+    <div>la requete n&apos;existe pas</div>
+  )
+}
+
+export default NotFoundPage
---
+++ app/users/[id]/not-found.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UserNotFoundPage = () => {
+  return (
+    <div>user n&apos;existe pas</div>
+  )
+}
+
+export default UserNotFoundPage
